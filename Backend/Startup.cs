57f17ebd6ff5cf7767ffc186853787d001eba7f5
raulbuffon <raulbuffon1999@gmail.com>
2021-05-10 22:58:54 -0300

    Add Swashbuckle package to use Swagger

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -26,6 +26,7 @@ namespace market_system
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,13 @@ namespace market_system
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
